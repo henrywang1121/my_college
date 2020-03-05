cf8de744750c2cf98998b6d308236ece40d880d3
God <god@M014010001208.v4.enabler.ne.jp>
2020-03-05 15:08:36 -0800

    built pages and blogs

--- footer.php
+++ footer.php
@@ -13,29 +13,36 @@
             <div class="site-footer__col-two">
                 <h3 class="headline headline--small">Explore</h3>
                 <nav class="nav-list">
+                    <!--
                     <?php wp_nav_menu(array(
                         'theme_location' => 'footerLocationOne'
                     ));
                     ?>
+                    -->
 
-                    <!--<ul>
+                    <ul>
                         <li><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
                         <li><a href="#">Programs</a></li>
                         <li><a href="#">Events</a></li>
                         <li><a href="#">Campuses</a></li>
-                        </ul>
-                    -->
+                    </ul>
                 </nav>
             </div>
 
             <div class="site-footer__col-three">
                 <h3 class="headline headline--small">Learn</h3>
                 <nav class="nav-list">
-                <ul>
-                    <li><a href="#">Legal</a></li>
-                    <li><a href="<?php echo site_url('/privacy-policy') ?>">Privacy</a></li>
-                    <li><a href="#">Careers</a></li>
-                </ul>
+                    <!--
+                    <?php wp_nav_menu(array(
+                            'theme_location' => 'footerLocationTwo'
+                        ));
+                    ?>
+                    -->
+                    <ul>
+                        <li><a href="#">Legal</a></li>
+                        <li><a href="<?php echo site_url('/privacy-policy') ?>">Privacy</a></li>
+                        <li><a href="#">Careers</a></li>
+                    </ul>
                 </nav>
             </div>
             </div>
@@ -43,11 +50,6 @@
             <div class="site-footer__col-four">
             <h3 class="headline headline--small">Connect With Us</h3>
             <nav>
-                <?php wp_nav_menu(array(
-                        'theme_location' => 'footerLocationTwo'
-                    ));
-                ?>
-                <!--
                 <ul class="min-list social-icons-list group">
                     <li><a href="#" class="social-color-facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
                     <li><a href="#" class="social-color-twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
@@ -55,7 +57,6 @@
                     <li><a href="#" class="social-color-linkedin"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
                     <li><a href="#" class="social-color-instagram"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
                 </ul>
-                -->
             </nav>
             </div>
         </div>
